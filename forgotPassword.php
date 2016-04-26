520c4906ad66e045275a4ebe87d4d726742adca6
Neil García Vargas <Neil@Neils-MacBook-Pro.local>
2016-04-25 23:46:37 -0600

    admin functionality

---
+++ forgotPassword.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<html>
+<head><title>Olvido contraseña</title>
+</head>
+<body>
+<p>Ingresa el correo con el que te encuentras en la plataforma </p>
+<form action="cgi-bin/forgotpassword-cgi" method="POST">
+Correo electronico: <input type="text" name="email"><br>
+<input type="submit" value="Send">
+</form>
+</body>
+</html>
