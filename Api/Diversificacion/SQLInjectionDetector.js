a49edc8ec95072a581df0624d8581ad333567825
Neil García Vargas <Neil@Neils-MacBook-Pro.local>
2016-05-28 09:46:49 -0600

    Diversificatio in login method

---
+++ Api/Diversificacion/SQLInjectionDetector.js
@@ -0,0 +1,72 @@
+var _ = require('lodash');
+var tokenGenerator = require('../TokenGenerator.js');
+var stringUtility = require('StringUtility.js');
+
+var SQLCommandsDictionary = ["ALTER","ANALYZE","BACKUP","CHANGE","CHECK","COMMIT","CREATE","DELETE","DESCRIBE","DO","DROP","EXPLAIN","FLUSH","GRANT","HANDLER","INSERT","JOIN","KILL","LOAD","FROM","TABLE","RENAME","REPLACE","REVOKE",
+"SELECT","SET","SHOW","START","STOP","TRUNCATE","UNION","USE","WHERE","ORDER","BY"];
+
+
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+      ? args[number]
+      : match
+    ;
+  });
+};
+
+module.exports = {
+  function generateTokenizedQuery(query)
+  {
+    var queryArray = stringToArray(query);
+    var token = tokenGenerator.generateToken();
+    var tokenizedString = "";
+    for (var i = 0; i< queryArray.length;i++)
+    {
+      if  (_.findIndex(SQLCommandsDictionary, queryArray[i]) > 0)
+      {
+        tokenizedString= tokenizedString + queryArray[i]+token+" ";
+      }else {
+
+        tokenizedString = tokenizedString+queryArray[i]+" ";
+      }
+    }
+    return {tokenizedQuery:tokenizedString, token:token};
+  }
+
+  function checkIfIsValidQuery(query, token)
+  {
+    var queryArray = stringToArray(query);
+    for (var i = 0; i< queryArray.length;i++)
+    {
+       var tokenStartPosition = queryArray[i].indexOf(token);
+
+       //If is not a tokenized command or string;
+       if (tokenStartPosition > -1)
+       {
+         //Check if this command should be tokenized, if should return false, because is a invalid (SQL injected) query.
+         if  (_.findIndex(SQLCommandsDictionary, queryArray[i]) > 0)
+         {
+           return false;
+         }
+       }
+    }
+    return true;
+  }
+
+
+  /**
+   * Return a vector with words that contains the string.
+   * @param  {[type]} string string
+   * @return {[type]}        [description]
+   */
+  function stringToArray(string)
+  {
+    return string.match(/\S+/g);
+  }
+
+
+
+
+}
