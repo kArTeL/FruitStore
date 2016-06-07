de688e243c0562083fa93827303b1d75ac8ea920
jgabrielgv <jgmezvargas@gmail.com>
2016-06-06 18:36:11 -0600

    Editando funcionalidad para probar api/fruits

--- Api/fruit/fruit.controller.js
+++ Api/fruit/fruit.controller.js
@@ -1,61 +1,63 @@
-  /**
-   * Using Rails-like standard naming convention for endpoints.
-   * GET     /things              ->  index
-   * POST    /things              ->  create
-   * GET     /things/:id          ->  show
-   * PUT     /things/:id          ->  update
-   * DELETE  /things/:id          ->  destroy
-   */
+/**
+ * Using Rails-like standard naming convention for endpoints.
+ * GET     /things              ->  index
+ * POST    /things              ->  create
+ * GET     /things/:id          ->  show
+ * PUT     /things/:id          ->  update
+ * DELETE  /things/:id          ->  destroy
+ */
 
-  'use strict';
-  var mysql = require('mysql');
-  var connection = require('../Connection.js');
-  var sessionValidator = require('../SessionValidator.js');
-  var fruitBuyer = require('./ProductOperation.js');
+'use strict';
+var mysql = require('mysql');
+var connection = require('../Connection.js');
+var sessionValidator = require('../SessionValidator.js');
+var fruitBuyer = require('./ProductOperation.js');
+var loggerBuilder = require('../LoggerBuilder.js');
+var tokenGenerator = require('../TokenGenerator.js');
+var sqlInjectionDetector = require('../Diversificacion/SQLInjectionDetector.js');
 
-  // Get list of holes
-  exports.index = function(req, res) {
-        connection.connection(function (err, conn) {
-          var json = req.body;
-          console.log(json);
-          if (!err) {
-            //check if the session is valid, this check the expirationDate, username,password,token
-            sessionValidator.validateSession(conn,json,function (error, success) {
+var logger = loggerBuilder.getLogger();
 
-              //if the session is valid go ahead
-              if (!error) {
-                conn.query("SELECT * FROM fruit",
-                   null,
-                   function(err, results) {
-                     conn.end(function(err) {
-                       console.log("error disconnecting the connection");
-                       console.log(err);
-                     });
-                     if (!err) {
-                       console.log(results);
-                        res.send(JSON.stringify(results));
-                     }
-                     else
-                     {
-                       return res.status(500).send({code:500, message:"Internal server error (sure)"});
-
-                     }
-                 });
-
-
+// Get list of holes
+exports.index = function (req, res) {
+  connection.connection(function (err, conn) {
+    var json = { "token": req.param('token'), "userId": req.param('userId') };
+    if (!err) {
+      //check if the session is valid, this check the expirationDate, username,password,token
+      var formatedAndTokenizedQueryString = "";
+      sessionValidator.validateSession(conn, json, formatedAndTokenizedQueryString, function (error, success) {
+        //if the session is valid go ahead
+        if (!error) {
+          logger.info(formatedAndTokenizedQueryString + ", inyectado");
+          connection.query("SELECT * FROM fruit",
+            null,
+            function (err, results) {
+              conn.end(function (err) {
+                console.log("error disconnecting the connection");
+                console.log(err);
+              });
+              conn.release(function (err) { });
+              if (!err) {
+                console.log(results);
+                res.send(JSON.stringify(results));
               }
-              else
-              {
-                return res.status(401).send(error);
-
+              else {
+                return res.status(500).send({ code: 500, message: "Internal server error (sure)" });
               }
             });
-          }
-          else {
-             return res.status(500).send({code:500, message:"Internal server error (sure)"});
-          }
-        });
-  };
+        }
+        else {
+          conn.release(function (err) { });
+          logger.info(formatedAndTokenizedQueryString + ", no valido");
+          return res.status(401).send(error);
+        }
+      });
+    }
+    else {
+      return res.status(500).send({ code: 500, message: "Internal server error (sure)" });
+    }
+  });
+};
 
   //Method need a post like this {creditcard: "" , userId: "",  products: [{fruitId:"", quantity: 2}]}
 exports.buyFruits = function (req, res) {
@@ -64,7 +66,8 @@ exports.buyFruits = function (req, res) {
 
   connection.connection(function (err, conn) {
     if (!err) {
-      sessionValidator.validateSession(conn,json,function (error, success) {
+      var formatedAndTokenizedQueryString = "";
+      sessionValidator.validateSession(conn,json,formatedAndTokenizedQueryString,function (error, success) {
         //if not error
         if (!error) {
 
